b51cef85c967dec43b00804e6e9a1ca7c71f2ae0
Nikhil Varma <nikhil_varma@hotmail.com>
2015-05-17 05:22:44 +0530

    Initial Commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+
+/**
+ * Created by Nikhil on 17-May-15.
+ */
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
